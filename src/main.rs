25c5dba58ab8f0223bec82206a291f5d6f3ff460
GLinBoy <glinboy@live.com>
2024-05-08 18:56:40 +0200

    👌 IMPROVE: Read user input line and convert it to the number

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,16 @@ fn main() {
     println!("4. Quite");
     println!("-----------------------------");
     print!("Select an option: ");
+
+    let mut line = String::new();
+
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read from stdin.");
+
+    let trimmed = line.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => println!("Your integer input: {}", i),
+        Err(..) => println!("This was not an integer: {}", trimmed),
+    };
 }
