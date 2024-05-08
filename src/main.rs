dce87b6f99d1267a62f1686fcfa564d6fd0bd69d
GLinBoy <glinboy@live.com>
2024-05-08 18:56:20 +0200

    👌 IMPROVE: Show options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("1. Add new entry");
+    println!("2. Explorer entries");
+    println!("3. Delete an entry");
+    println!("4. Quite");
+    println!("-----------------------------");
+    print!("Select an option: ");
 }
